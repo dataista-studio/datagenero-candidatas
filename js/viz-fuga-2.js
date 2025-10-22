d58b7adb5695f14644ed6d370075a482ec82928b
Fernando Becerra <becerrafernando@gmail.com>
2025-10-21 22:35:22 -0300

    horizontal arrangement for viz

--- js/viz-fuga-2.js
+++ js/viz-fuga-2.js
@@ -1,12 +1,13 @@
-function makeChart(data, index) {
+function drawChart(data, index) {
     const field = "jurisdicción"
     const jurisdicciones = Array.from(new Set(data.map(d => d[field])));
 
-    const margin4 = ({ top: 20, right: 20, bottom: 10, left: 120 });
+    const margin4 = ({ top: 120, right: 20, bottom: 10, left: 20 });
 
-    const width4 = 500;
-    const height4 = jurisdicciones.length * 26;
+    const width4 = jurisdicciones.length * 26;
+    const height4 = 500;
     const chartWidth4 = width4 - margin4.left - margin4.right;
+    const chartHeight4 = height4 - margin4.top - margin4.bottom;
 
     // Create a SVG container.
     const svg4 = d3.select(`#viz02-0${index}`)
@@ -17,13 +18,13 @@ function makeChart(data, index) {
 
     const g4 = svg4.append("g");
 
-    const yScale4 = d3.scaleBand()
+    const xScale4 = d3.scaleBand()
         .padding(0.05)
-        .range([margin4.top, height4 - margin4.bottom])
+        .range([margin4.left, width4 - margin4.right])
         .domain(jurisdicciones);
 
     const padding = 8;
-    const candWidth = (chartWidth4 * 2/3 - 4 * padding) / 2;
+    const candWidth = (chartHeight4 * 2/3 - 4 * padding) / 2;
 
     const listaRect = g4.selectAll(".lista")
       .data(["mayoria", "minoria"])
@@ -59,27 +60,34 @@ function makeChart(data, index) {
       .data(jurisdicciones)
       .join("g")
         .attr("class", "juri")
-        .attr("transform", d => `translate(0, ${yScale4(d)})`);
+        .attr("transform", d => `translate(${xScale4(d)}, 0)`);
 
     j.selectAll(".label")
       .data(d => [d])
       .join("text")
-        .attr("x", 0)
-        .attr("y", yScale4.bandwidth() / 2 + 4)
+        .attr("x", xScale4.bandwidth() / 2)
+        .attr("y", margin4.top / 2)
         .text(d => d)
 
     const candidatxs = j.selectAll(".cand")
       .data(d => data.filter(f => f[field] === d))
-      .join('rect')
+      .join('circle')
       .attr("class", "cand")
-        .attr("x", (d, i) => i === 0 ? margin4.left + padding / 2 : 
+        // .attr("x", (d, i) => i === 0 ? margin4.left + padding / 2 : 
+        //   (i === 1 
+        //     ? margin4.left + padding + candWidth + 2 * padding 
+        //     : margin4.left + chartWidth4 * 2/3 + padding)
+        // )
+        // .attr("y", 5)
+        // .attr("width", candWidth)
+        // .attr("height", yScale4.bandwidth() - 10)
+        .attr("cy", (d, i) => i === 0 ? margin4.top + padding / 2 : 
           (i === 1 
-            ? margin4.left + padding + candWidth + 2 * padding 
-            : margin4.left + chartWidth4 * 2/3 + padding)
+            ? margin4.top + padding + candWidth + 2 * padding 
+            : margin4.top + chartHeight4 * 2/3 + padding)
         )
-        .attr("y", 5)
-        .attr("width", candWidth)
-        .attr("height", yScale4.bandwidth() - 10)
+        .attr("cx", xScale4.bandwidth() / 2)
+        .attr("r", 22.5)
         .style("fill", d => d.genero === '' ? 'lightgray'
           : (d.genero === "femenino" ? 'pink' : 'yellow')
         )
@@ -125,7 +133,7 @@ Promise.all([
     const indices = d3.range(2, 6);
 
     indices.forEach(idx => {
-        makeChart(fe, idx);
+        drawChart(fe, idx);
     })
 
 })
