b648cb04380fd296d1c97aeafe3ad79d478fca9a
Fernando Becerra <becerrafernando@gmail.com>
2025-11-09 17:41:34 -0300

    add colores and iterate over bircirgles

--- js/viz-fuga-4.js
+++ js/viz-fuga-4.js
@@ -13,116 +13,53 @@ function drawCirclePack(data, svgId) {
           .attr("viewBox", [0, 0, width2, height2])
           .attr("style", "max-width: 100%;");
 
+    data.forEach((bigCircle, idx) => {
+        const radii = bigCircle.radius / 12;
+        const padding = 10;
 
-    const currentRadius = height2 / 2.5;
-    const circleRadius = currentRadius / 0.9;
-    const radii = circleRadius / 14;
+        svg2.append("circle")
+            .attr("cx", bigCircle.x)
+            .attr("cy", bigCircle.y)
+            .attr("r", bigCircle.radius)
+            .attr("fill", "#544059");
 
-    svg2.append("circle")
-        .attr("cx", width2 / 2)
-        .attr("cy", height2 / 2)
-        .attr("r", circleRadius)
-        .attr("fill", "#544059");
+        const mujeres = d3.range(bigCircle.mujeres).map(() => ({ color: colorMujer}));
+        const hombres = d3.range(bigCircle.hombres).map(() => ({ color: colorHombre}));
+        const cand = d3.shuffle([...mujeres, ...hombres]);
 
-    const padding = 10;
+        const circles = cand.map((d, i) => {
+            const angle = Math.PI * (3 - Math.sqrt(5));
+            const r = (radii + padding) * 1.2 * Math.sqrt(i + 1);
+            const a = (i + 1) * angle;
+            const x = r * Math.cos(a);
+            const y = r * Math.sin(a);
+            return {
+                x: x,
+                y: y,
+                color: d.color
+            }
+        })
 
-    // const circles = d3.packSiblings(d3.range(30).map(() => ({r: radii + padding + Math.random()})));
-    const circles = d3.range(30).map((i) => {
-        const angle = Math.PI * (3 - Math.sqrt(5));
-        const r = (radii + padding) * 1.2 * Math.sqrt(i + 1);
-        const a = (i + 1) * angle;
-        const x = r * Math.cos(a);
-        const y = r * Math.sin(a);
-        return {
-            x: x,
-            y: y
-        }
-    })
-    console.log(circles)
+        const g2 = svg2.append("g")
 
-    const g2 = svg2.append("g")
-
-    g2.selectAll('circle')
-        .data(circles)
-        .join("circle")
-            .attr("cx", d => width2 / 2 + d.x)
-            .attr("cy", d => height2 / 2 + d.y)
-            .attr("r", radii)
-            .attr("fill", "white")
-
-    // build the one level hierarchy
-    // const rootChildren = Array.from({ length: 30 }).map(
-    //     (m, i) => (m = { name: i })
-    // );
-    // const rootData = { name: "root", children: rootChildren };
-    // const root = d3.hierarchy(rootData);
-    // root.count();
-    // // apply the pack
-    // d3
-    //     .pack()
-    //     .size([currentRadius * 1.8, currentRadius * 1.8])
-    //     .padding(0)(root);
-    //     console.log(root)
-
-    // // build new data with packX + packY and centre node fixed with radius of the rest * 2
-    // const circleData = root.children.reduce((acc, descendant, i) => {
-    //     const newDescendant = descendant;
-    //     newDescendant.packX = descendant.x;
-    //     newDescendant.packY = descendant.y;
-    //     acc.push(newDescendant);
-    //     return acc;
-    // }, []);
-
-    // const packShift =
-    //     (height2 - currentRadius * 2) / 2 + currentRadius * 0.1;
-
-    // const circleGroup = svg2
-    //     .selectAll(".circleGroup")
-    //     .data(root.children)
-    //     .join((group) => {
-    //         const enter = group.append("g").attr("class", "circleGroup");
-    //         enter.append("circle").attr("class", "packCircle");
-
-    //         return enter;
-    //     });
-
-    // circleGroup.attr("transform", `translate(${packShift},${packShift})`);
-
-    // circleGroup
-    //     .select(".packCircle")
-    //     .attr("fill", (d) => (d.depth === 0 ? "red" : "white"))
-    //     .attr("stroke-width", 0)
-    //     .attr("r", (d) => d.r * 0.8);
-
-    // const simulation = d3
-    //     .forceSimulation()
-    //     .force("x", d3.forceX((d) => d.packX))
-    //     .force("y", d3.forceY((d) => d.packY))
-    //     .force(
-    //     "collide",
-    //     d3
-    //         .forceCollide()
-    //         .radius((d) => d.r * 1.2)
-    //         .iterations(50)
-    //         .strength(1)
-    //     )
-    //     .on("tick", () => {
-    //     circleGroup
-    //         .selectAll(".packCircle")
-    //         .attr("cx", (d) => d.x)
-    //         .attr("cy", (d) => d.y);
-    //     });
-
-    // simulation.stop();
-    // simulation.nodes(circleData);
-    // simulation.alpha(1).restart();
-    
+        g2.selectAll(`.small-circle-${idx}`)
+            .data(circles)
+            .join("circle")
+                .attr("class", `.small-circle-${idx}`)
+                .attr("cx", d => width2 / 2 + d.x)
+                .attr("cy", d => height2 / 2 + d.y)
+                .attr("r", radii)
+                .attr("fill", d => d.color);
+        })
 }
 
 const circles = [
     {
         'mujeres': 15,
-        'hombres': 15
+        'hombres': 15,
+        'radius': 300,
+        'x': 300, 
+        'y': 300,
     }
 ]
 
