a3986c30eaacb934f4d5980b34223d5c75cd5f29
Fernando Becerra <becerrafernando@gmail.com>
2025-11-13 23:42:53 -0300

    tooltip for mobile

--- js/candidatas.js
+++ js/candidatas.js
@@ -61,13 +61,17 @@ function transitionDropDots(index, divId) {
 
 function transitionMarimekko(index, divId) {
     if (index === getDivIndexInStep(divId)) {
-        console.log(divId)
 
         const svg = d3.select(`#${divId}`).select("svg");
         const toolDiv = d3.select(`#${divId}`).select(".viz-tooltip");
 
         const onClick = (evt, d) => {
             const [x,y] = d3.pointer(evt);
+            const nombre = d.distrito === 'santiago del estero'
+                ? 'Stgo. del Estero' : (
+                    d.distrito === 'tierra del fuego aias'
+                    ? "T. del F. AIAS" : d.distrito
+                )
             toolDiv.style("display", "block")
                 .html(`
                     <div id="close">
@@ -75,7 +79,7 @@ function transitionMarimekko(index, divId) {
                         <path d="M17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289Z" fill="#302235"/>
                         </svg>
                     </div>
-                    <span class="tooltip-title">${d.distrito}</span>
+                    <span class="tooltip-title">${nombre}</span>
                     <p>Listas encabezadas por mujeres: <span class="tooltip-number">${(d['% mujeres']*100).toFixed(0)}%</span></p>
                 `);
 
